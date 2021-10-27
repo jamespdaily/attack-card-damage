f629d2d3a97943314d162e572ef2bc58846a939f
Jim Daily <jaypeedaylee@gmail.com>
2021-10-27 18:18:03 -0400

    attemped correction of renderChatMessage hook

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -23,6 +23,6 @@ class AttackCardDamage {
     registerPackageDebugFlag(AttackCardDamage.ID);
   });
 
-  Hooks.on('renderChatMessage', ({ ChatMessagePF2e }) => {
-      console.log(ChatMessagePF2e.data.flags.pf2e.context.type);
+  Hooks.on('renderChatMessage', ({ message, html, data }) => {
+      console.log(message.data.flags.pf2e.context.type);
   });
