8d3035ff20550449d2935a53c7276a701ce89447
Jim Daily <jaypeedaylee@gmail.com>
2021-10-27 15:58:11 -0400

    testing hook

---
+++ scripts/attack-card-damage.js
@@ -0,0 +1,28 @@
+class AttackCardDamage {
+    static ID = 'attack-card-damage';
+
+    /**
+     * A small helper function which leverages developer mode flags to gate debug logs.
+     * 
+     * @param {boolean} force - forces the log even if the debug flag is not on
+     * @param  {...any} args - what to log
+     */
+    static log(force, ...args) {  
+      const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
+  
+      if (shouldLog) {
+        console.log(this.ID, '|', ...args);
+      }
+    }
+  }
+  
+  /**
+   * Register our module's debug flag with developer mode's custom hook
+   */
+  Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
+    registerPackageDebugFlag(AttackCardDamage.ID);
+  });
+
+  Hooks.on('renderChatMessage', ({ message, html, data }) => {
+      console.log('oh fuck');
+  });
