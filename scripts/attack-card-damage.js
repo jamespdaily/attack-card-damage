cf41d42a52c038591378e244611c52b74ff67fce
Jim Daily <jaypeedaylee@gmail.com>
2021-10-28 09:32:49 -0400

    click actions first attempt

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -1,8 +1,21 @@
 Hooks.on('renderChatMessage', (message, html, data) => {
   if (typeof message.data.flags.pf2e.context !== 'undefined') {
     if (message.data.flags.pf2e.context.type === 'attack-roll') {
-      const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
+      const damageButton = $(`<button type="button" data-action="strikeDamage">Damage</button>`);
+      const criticalButton = $(`<button type="button" data-action="strikeCritical">Critical</button>`);
+
+      //const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
+      const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"></div></div>`);
+
+      damageButtons.find('.card-buttons-two-column').append(damageButton);
+      damageButtons.find('.card-buttons-two-column').append(criticalButton);
+
       const attackRollElement = html.find('.dice-roll').after(damageButtons);
+
+      damageButton.click(e => {
+        e.stopPropagation();
+        console.log('clicky');
+      })
     }
   }
 });
