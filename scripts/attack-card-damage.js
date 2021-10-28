a4532beefa49f397dba731b29e4ca9dafe117b3d
Jim Daily <jaypeedaylee@gmail.com>
2021-10-28 08:31:26 -0400

    check for pf2e context

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -1,5 +1,5 @@
 Hooks.on('renderChatMessage', (message, html, data) => {
-  if (typeof message.data.flags.pf2e.context.type !== 'undefined') {
+  if (typeof message.data.flags.pf2e.context !== 'undefined') {
     if (message.data.flags.pf2e.context.type === 'attack-roll') {
       const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
       const attackRollElement = html.find('.dice-roll').after(damageButtons);
