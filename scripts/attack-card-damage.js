521ae17c31cf30f8be216e1c7103185b63ed43ca
Jim Daily <jaypeedaylee@gmail.com>
2021-10-27 23:15:05 -0400

    let's log some shit

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -1,7 +1,8 @@
-Hooks.on('renderChatMessage', (message) => {
+Hooks.on('renderChatMessage', (message, html, data) => {
   if (!message.data.flags.pf2e.context.type) return;
 
   if (message.data.flags.pf2e.context.type === 'attack-roll') {
     console.log(message.data.flags.pf2e.context.type);
+    console.log(html);
   }
 });
