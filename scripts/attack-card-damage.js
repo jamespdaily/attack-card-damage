ce58cb12831f6ee3e7c52047e7d731766edfebff
Jim Daily <jaypeedaylee@gmail.com>
2021-10-27 23:32:05 -0400

    appending stuff

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -2,7 +2,8 @@ Hooks.on('renderChatMessage', (message, html, data) => {
   if (!message.data.flags.pf2e.context.type) return;
 
   if (message.data.flags.pf2e.context.type === 'attack-roll') {
-    console.log(message.data.flags.pf2e.context.type);
-    console.log(html);
+    const attackRollElement = html.find('.dice-total').append('asdfasdf');
   }
 });
+
+
