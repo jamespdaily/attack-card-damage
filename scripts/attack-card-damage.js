02926215e65ddf84f38c697b53f073d9006cda56
Jim Daily <jaypeedaylee@gmail.com>
2021-10-28 10:32:48 -0400

    testing the button functionality

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -4,7 +4,6 @@ Hooks.on('renderChatMessage', (message, html, data) => {
       const damageButton = $(`<button type="button" data-action="strikeDamage">Damage</button>`);
       const criticalButton = $(`<button type="button" data-action="strikeCritical">Critical</button>`);
 
-      //const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
       const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"></div></div>`);
 
       damageButtons.find('.card-buttons-two-column').append(damageButton);
@@ -13,6 +12,11 @@ Hooks.on('renderChatMessage', (message, html, data) => {
       const attackRollElement = html.find('.dice-roll').after(damageButtons);
 
       damageButton.click(e => {
+        e.stopPropagation();
+        actor.data.data.actions.find((action) => action.id === weapon.id).damage({event});
+      })
+
+      criticalButton.click(e => {
         e.stopPropagation();
         console.log('clicky');
       })
