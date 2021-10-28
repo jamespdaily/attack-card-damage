e880d55f9681d3ab905560095c61ed6bdafcd41f
Jim Daily <jaypeedaylee@gmail.com>
2021-10-27 23:46:28 -0400

    try appending html

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -2,7 +2,8 @@ Hooks.on('renderChatMessage', (message, html, data) => {
   if (!message.data.flags.pf2e.context.type) return;
 
   if (message.data.flags.pf2e.context.type === 'attack-roll') {
-    const attackRollElement = html.find('.dice-total').append('asdfasdf');
+    const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
+    const attackRollElement = html.find('.dice-total').append(damageButtons);
   }
 });
 
