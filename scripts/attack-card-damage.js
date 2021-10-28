49c9837c8b2a81ebefb632f4c8e32abc44960006
Jim Daily <jaypeedaylee@gmail.com>
2021-10-28 00:22:00 -0400

    move div tag

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -3,8 +3,6 @@ Hooks.on('renderChatMessage', (message, html, data) => {
 
   if (message.data.flags.pf2e.context.type === 'attack-roll') {
     const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
-    const attackRollElement = html.find('.dice-total').append(damageButtons);
+    const attackRollElement = html.find('.dice-roll').append(damageButtons);
   }
 });
-
-
