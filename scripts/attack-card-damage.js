ad936a456a82849a4127bd9caa70a7b08c51ef48
Jim Daily <jaypeedaylee@gmail.com>
2021-10-28 00:45:18 -0400

    trying to skip type errors

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -1,8 +1,8 @@
 Hooks.on('renderChatMessage', (message, html, data) => {
-  if (!message.data.flags.pf2e.context.type) return;
-
-  if (message.data.flags.pf2e.context.type === 'attack-roll') {
-    const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
-    const attackRollElement = html.find('.dice-roll').after(damageButtons);
+  if (typeof message.data.flags.pf2e.context.type !== 'undefined') {
+    if (message.data.flags.pf2e.context.type === 'attack-roll') {
+      const damageButtons = $(`<div class="pf2e chat-card action-card"> <div class="card-buttons" data-visibility="owner"> <div class="card-buttons-two-column"> <button type="button" data-action="strikeDamage">Damage</button> <button type="button" data-action="strikeCritical">Critical</button> </div></div>`);
+      const attackRollElement = html.find('.dice-roll').after(damageButtons);
+    }
   }
 });
