ebf5fa696c1a6591c21551267c78a54cda2050f4
Jim Daily <jaypeedaylee@gmail.com>
2021-10-27 21:27:45 -0400

    omfg

--- scripts/attack-card-damage.js
+++ scripts/attack-card-damage.js
@@ -1,28 +1,3 @@
-class AttackCardDamage {
-    static ID = 'attack-card-damage';
-
-    /**
-     * A small helper function which leverages developer mode flags to gate debug logs.
-     * 
-     * @param {boolean} force - forces the log even if the debug flag is not on
-     * @param  {...any} args - what to log
-     */
-    static log(force, ...args) {  
-      const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(this.ID);
-  
-      if (shouldLog) {
-        console.log(this.ID, '|', ...args);
-      }
-    }
-  }
-  
-/**
- * Register our module's debug flag with developer mode's custom hook
- */
-Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
-  registerPackageDebugFlag(AttackCardDamage.ID);
-});
-
 Hooks.on('renderChatMessage', (message) => {
   console.log(message.data.flags.pf2e.context.type);
 });
