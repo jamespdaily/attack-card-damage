9285972cdb3521ec021ac68f98580a404a568fbd
Jim Daily <jaypeedaylee@gmail.com>
2022-03-18 12:47:31 -0400

    add popup to tell fools to remove this rig

--- src/module/preloadTemplates.ts
+++
@@ -1,5 +0,0 @@
-export async function preloadTemplates(): Promise<Handlebars.TemplateDelegate[]> {
-  const templatePaths: string[] = ['modules/attack-card-damage/templates/damage-buttons.html'];
-
-  return loadTemplates(templatePaths);
-}
